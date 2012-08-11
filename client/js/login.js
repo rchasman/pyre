ca6b3f7318a5421f904ce560e53f5fbe6e99f0b3
Roey Chasman <rchasman@email.arizona.edu>
2012-08-11 11:04:53 -0700

    Style improvements.

--- client/js/login.js
+++ client/js/login.js
@@ -3,12 +3,14 @@ Template.logged_in.events = {"click #login-submit":function() {
     username = $("#login-user").val();
     password = $("#login-pass").val();
     result = Users.findOne({username: username, password: password});
-    if(result)
+    if(result){
         Session.set("user_id", result._id)
         Session.set("user_name", result.username)
+    } else {
+        $('#pw-error').addClass("label label-important").html("Invalid Username/Password");
+    }
 }};
 
 Template.logged_in.username = function () {
-    //return Session.get("user_name");
-    return "rchasman";
+    return Session.get("user_name");
 };
