bd30a1445ae5943c36583cc087d023c7a868af8c
Chris LaRose <cjlarose@gmail.com>
2012-08-11 09:36:37 -0700

    Tasks working

---
+++ client/js/task_item.js
@@ -0,0 +1,78 @@
+Template.todo_item.tag_objs = function () {
+  var todo_id = this._id;
+  return _.map(this.tags || [], function (tag) {
+    return {todo_id: todo_id, tag: tag};
+  });
+};
+
+Template.todo_item.done_class = function () {
+  return this.done ? 'done' : '';
+};
+
+Template.todo_item.done_checkbox = function () {
+  return this.done ? 'checked="checked"' : '';
+};
+
+Template.todo_item.editing = function () {
+  return Session.equals('editing_itemname', this._id);
+};
+
+Template.todo_item.adding_tag = function () {
+  return Session.equals('editing_addtag', this._id);
+};
+
+Template.todo_item.events = {
+  'click .check': function () {
+    Todos.update(this._id, {$set: {done: !this.done}});
+  },
+
+  'click .destroy': function () {
+    Todos.remove(this._id);
+  },
+
+  'click .addtag': function (evt) {
+    Session.set('editing_addtag', this._id);
+    Meteor.flush(); // update DOM before focus
+    focus_field_by_id("edittag-input");
+  },
+
+  'dblclick .display .todo-text': function (evt) {
+    Session.set('editing_itemname', this._id);
+    Meteor.flush(); // update DOM before focus
+    focus_field_by_id("todo-input");
+  },
+
+  'click .remove': function (evt) {
+    var tag = this.tag;
+    var id = this.todo_id;
+
+    evt.target.parentNode.style.opacity = 0;
+    // wait for CSS animation to finish
+    Meteor.setTimeout(function () {
+      Todos.update({_id: id}, {$pull: {tags: tag}});
+    }, 300);
+  }
+
+};
+
+Template.todo_item.events[ okcancel_events('#todo-input') ] =
+  make_okcancel_handler({
+    ok: function (value) {
+      Todos.update(this._id, {$set: {text: value}});
+      Session.set('editing_itemname', null);
+    },
+    cancel: function () {
+      Session.set('editing_itemname', null);
+    }
+  });
+
+Template.todo_item.events[ okcancel_events('#edittag-input') ] =
+  make_okcancel_handler({
+    ok: function (value) {
+      Todos.update(this._id, {$addToSet: {tags: value}});
+      Session.set('editing_addtag', null);
+    },
+    cancel: function () {
+      Session.set('editing_addtag', null);
+    }
+  });
