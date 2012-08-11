99bf1843da1ed9f75aff2e4e966c2a65ae28b83f
Roey Chasman <rchasman@email.arizona.edu>
2012-08-10 23:50:40 -0700

    Defined domain specific collections.

--- server/publish.js
+++ server/publish.js
@@ -1,4 +1,4 @@
-// Lists -- {name: String}
+/* Lists -- {name: String}
 Lists = new Meteor.Collection("lists");
 
 // Publish complete set of lists to all clients.
@@ -17,5 +17,14 @@ Todos = new Meteor.Collection("todos");
 // Publish all items for requested list_id.
 Meteor.publish('todos', function (list_id) {
   return Todos.find({list_id: list_id});
+});*/
+
+Schools = new Meteor.Collection("schools");
+Meteor.publish('schools', function () {
+  return Schools.find();
 });
 
+Users = new Meteor.Collection("users");
+Meteor.publish('users', function () {
+  return Users.find();
+});
